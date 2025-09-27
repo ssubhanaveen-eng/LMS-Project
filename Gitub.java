fadea5e1a16884299e30e3cc4336046c697c19be
NaveenPradeen <naveens10699@gmail.com>
2025-09-27 10:48:52 +0530

    This is my First commit

---
+++ Gitub.java
@@ -0,0 +1,13 @@
+package com.gitun_project;
+
+public class Gitub {
+	public static void main(String[] args) {
+		System.out.println("Welcome Gitub");
+		//pwd -print working directory
+		//ls - list Files
+		//cd - change the current directory (cd src)
+		//cat - display file content
+		//git clone - copies a repository from a remote source
+	}
+
+}
