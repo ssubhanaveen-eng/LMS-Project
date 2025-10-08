4f18ce34238c1686053dbb113dd2b023215dc980
NaveenPradeen <naveens10699@gmail.com>
2025-10-08 13:59:29 +0530

    this is my second commit

---
+++ Github_repository.java
@@ -0,0 +1,20 @@
+package com.github_repository;
+
+public class Github_repository {
+	public void propert1() {
+		System.out.println("Land");
+	}
+	public void property2() {
+		System.out.println("Home");
+		
+	}
+	public void property3() {
+		System.out.println("House");
+	}
+public static void main(String[] args) {
+	Github_repository a = new Github_repository();
+	a.propert1();
+	a.property2();
+	a.property3();
+}
+}
